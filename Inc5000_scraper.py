d6606ad4a64a60350eaaaac31f7190beffc37602
pjdougherty <pjdougherty@outlook.com>
2015-08-21 10:36:12 -0400

    Scrape the Inc. 5000 list
    Uses Inc-designated company ids and the list's REST URL to extract and
    organize JSON records for each company. A total of 51 features are
    recorded, although more or less may be available depending on the number
    of times that they have been included on the Inc. 5000 list.

---
+++ Inc5000_scraper.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 08:39:57 2015
+
+@author: pdougherty
+"""
+
+import requests
+import pandas as pd
+
+def getInc5000(year):
+    # Create dictionary of Inc. 5000 years with the Inc-designated id of the top-ranked company in each year
+    year_dict = {2014:22890}
+    
+    # Set current_company_id equal to that of Fuhu, the #1 company in 2014
+    current_company_id = year_dict[year]
+    
+    '''
+    key_list = [u'app_revenues_fouryearsago', u'city_display_name', u'twitter', u'app_employ_fouryearsago', u'rank',
+                u'app_revenues_lastyear', u'linkedin', u'ifc_filelocation', u'id', u'app_employ_lastyear',
+                u'ifc_twitter_handle', u'industry_id', u'ifc_business_model', u'ifc_url', u'next_id',
+                u'description', u'ifc_state', u'ceo', u'metro_id', u'revenue_range', u'facebook', u'next_preview_text',
+                u'industry_rank', u'address', u'next_badge_url', u'ifc_city', u'ifi_industry', u'country',
+                u'ifc_company', u'state_rank', u'metro_rank', u'current_industry_rank', u'ifc_founded']
+    years_list_current = ['ify_state_rank_2014', 'ify_employee_count_2014', 'ify_metro_rank_2014', 'ify_employee_count_previous_2014',
+                             'ify_industry_rank_2014', 'ify_revenue_previous_2014', 'ify_year_2014', 'ify_rank_2014', 'ify_revenue_2014']
+    years_list_prev = ['ify_state_rank_2013', 'ify_employee_count_2013', 'ify_metro_rank_2013', 'ify_employee_count_previous_2013',
+                       'ify_industry_rank_2013', 'ify_revenue_previous_2013', 'ify_year_2013', 'ify_rank_2013', 'ify_revenue_2013']
+    '''
+    # Create empty lists for eventual pandas column names
+    key_list, years_list_current, years_list_prev = [], [], []
+    # Create empt list that all company data will be appended to
+    all_co_data_list = []
+    
+    # Loop through the Inc. 5000
+    # Company #5000 has no next_id value, so the loop will end
+    while current_company_id:
+        # Get company data as JSON
+        current_company = requests.get('http://www.inc.com/rest/inc5000company/'+str(current_company_id)+'/full_list').json()
+        
+        # Extract company data to lists
+        current_co_data = []
+        current_co_years_data = []
+        for key in current_company['data']:
+            if key != 'years':
+                current_co_data.append(current_company['data']['%s' % key])
+            elif key == 'years':
+                for k in current_company['data']['years'][0]:
+                    current_co_years_data.append(current_company['data']['years'][0]['%s' % k])
+                if len(current_company['data']['years']) > 1:
+                    for k in current_company['data']['years'][1]:
+                        current_co_years_data.append(current_company['data']['years'][1]['%s' % k])
+                    
+        # Set the current_company_id equal to the next id so it actually iterates
+        current_company_id = current_company['data']['next_id']
+                    
+        # Combine lists and add it to the list of lists holding all company data
+        company_data = current_co_data + current_co_years_data
+        all_co_data_list.append(company_data)
+        
+    # Makes a list of eventual pandas headers
+    key_list = list(current_company['data'].keys())
+    key_list.remove('years')
+    # Makes a list from nested elements of company data for more pandas headers
+    years_list_current = []
+    for i in list(current_company['data']['years'][0]):
+        years_list_current.append(str(i)+'_current')
+    if len(current_company['data']['years']) > 1:
+        years_list_prev = []
+        for i in current_company['data']['years'][1]:
+            years_list_prev.append(str(i)+'_prev')
+            
+    # Create a dataframe from the long list and transpose it
+    inc5000 = pd.DataFrame(all_co_data_list)
+    inc5000 = inc5000.T.reset_index(drop=True)
+    # Rename columns by simply replacing them with the list of columns created by the last company to loop
+    inc5000.columns = [key_list+years_list_current+years_list_prev]
+    
+    return inc5000
+    
+# Example: getInc5000(2014)
+    
+inc2014 = getInc5000(2014)
